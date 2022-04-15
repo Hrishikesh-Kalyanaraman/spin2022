8bfe18aec4b1b13bf53d8871088771d18c960fa6
Hrishikesh Kalyanaraman <hk39@illinois.edu>
2022-04-15 02:19:26 -0500

    Changed hello.py to return the git log history
    Made some additional changes in main.yaml(changed branch and ref to "solution")

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import pygit2;
 
-print("""<html>
+repo = pygit2.Repository('.git')
+
+print(""""<html>
 <body>
 <p>Hello, Roland!<p/>""")
 
@@ -11,6 +14,15 @@ TODO: add git log here
 </pre>
 </p>
 """)
+
+# check the last log (git log -1)
+"""commit = repo[repo.head.target]
+print(commit.message) """
+
+# command for git log
+last = repo[repo.head.target]
+for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
+    print(commit.message)
 
 print("""
 </body>
